206ca9e7a44913bbca8741112c06e446de303e93
Denis Atkesone <denis.atkesone@gmail.com>
2017-12-28 16:01:23 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,7 @@ def line(queue)
   if queue.size == 0
     puts "The line is currently empty."
   else
+    index = 0
     line = "The line is currently:"
     queue.each do |people, index|
       line << " #{index + 1}. #{people}"
